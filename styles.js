b56f67dfe1db91f09d1698c95d9828130d8f700a
Matheus Santoz <ninguemd75@gmail.com>
2025-10-15 23:53:25 -0300

    nav changes, mobile layout changes

--- styles.js
+++ styles.js
@@ -17,6 +17,57 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(target);
     });
 
+    const upperHeader = document.querySelector('.upper-header');
+    const nav = document.querySelector('nav');
+    const menuButton = document.getElementById('menu-clickable');
+    const closeButton = document.getElementById('exit-menu');
+    const shadow = document.getElementById('shadow-menu');
+    const menuContainer = document.getElementById('menu-collapse');
+    const body = document.body;
+
+    if (menuButton && closeButton && shadow && menuContainer) {
+
+        const openMenu = () => {
+            menuContainer.classList.add('menu-ativo');
+            body.classList.add('no-scroll');
+        };
+
+        const closeMenu = () => {
+            menuContainer.classList.remove('menu-ativo');
+            body.classList.remove('no-scroll');
+        };
+
+        menuButton.addEventListener('click', openMenu);
+        closeButton.addEventListener('click', closeMenu);
+        shadow.addEventListener('click', closeMenu);
+    }
+
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener('scroll', () => {
+
+        if (window.innerWidth <= 768) {
+            return;
+        }
+
+        if (body.classList.contains('no-scroll')) {
+            return;
+        }
+
+        const currentScrollY = window.scrollY;
+
+
+        if (currentScrollY > lastScrollY && currentScrollY > 105) {
+            if (upperHeader) upperHeader.style.transform = 'translateY(-25px)';
+            if (nav) nav.style.transform = 'translateY(-25px)';
+        } else if (currentScrollY < lastScrollY && currentScrollY == 0) {
+            if (upperHeader) upperHeader.style.transform = 'translateY(0)';
+            if (nav) nav.style.transform = 'translateY(0)';
+        }
+
+        lastScrollY = currentScrollY;
+    });
+
 });
 
 // o código abaixo controla a exibição do menu lateral e o comportamento de rolagem da página 
