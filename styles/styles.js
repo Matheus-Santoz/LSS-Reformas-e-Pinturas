333ac82dd6312dbc5f48e9f887d0ae0fdbc7a073
Matheus Santoz <ninguemd75@gmail.com>
2025-10-24 05:11:00 -0300

    services page done, footer done, gallery done

--- styles/styles.js
+++ styles/styles.js
@@ -17,29 +17,26 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(target);
     });
 
+    //comportamento do menu mobile
     const upperHeader = document.querySelector('.upper-header');
     const nav = document.querySelector('nav');
     const menuButton = document.getElementById('menu-clickable');
     const closeButton = document.getElementById('exit-menu');
-    const shadow = document.getElementById('shadow-menu');
     const menuContainer = document.getElementById('menu-collapse');
     const body = document.body;
 
-    if (menuButton && closeButton && shadow && menuContainer) {
+    if (menuButton && closeButton && menuContainer) {
 
         const openMenu = () => {
             menuContainer.classList.add('menu-ativo');
-            body.classList.add('no-scroll');
         };
 
         const closeMenu = () => {
             menuContainer.classList.remove('menu-ativo');
-            body.classList.remove('no-scroll');
         };
 
         menuButton.addEventListener('click', openMenu);
         closeButton.addEventListener('click', closeMenu);
-        shadow.addEventListener('click', closeMenu);
     }
 
     let lastScrollY = window.scrollY;
@@ -70,28 +67,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-// o código abaixo controla a exibição do menu lateral e o comportamento de rolagem da página 
-const btnMenu = document.getElementById("menu-clickable");
-const btnCloseMenu = document.getElementById("exit-menu");
-const menuContent = document.getElementById("menu-collapse");
-const containerScroll = document.getElementById("body");
-const shadowMenu = document.getElementById("shadow-menu");
 
-btnMenu.addEventListener("click", () => {
-    menuContent.classList.add("menu-ativo");
-    shadowMenu.classList.add("menu-ativo");
-    containerScroll.classList.add("no-scroll");
-});
-
-btnCloseMenu.addEventListener("click", () => {
-    menuContent.classList.remove("menu-ativo");
-    shadowMenu.classList.remove("menu-ativo");
-    containerScroll.classList.remove("no-scroll");
-});
-
-shadowMenu.addEventListener("click", () => {
-    menuContent.classList.remove("menu-ativo");
-    shadowMenu.classList.remove("menu-ativo");
-    containerScroll.classList.remove("no-scroll");
-});
 
